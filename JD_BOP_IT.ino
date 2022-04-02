1bb191ebf5df7013be2ac1af0c783e6ebd5dc9a8
tamer-muddi <94093890+tamer-muddi@users.noreply.github.com>
2022-04-02 13:14:45 -0400

    Add files via upload
    This code detects the rotation for the joystick, I still need to add the time limit to it.

---
+++ JD_BOP_IT.ino
@@ -0,0 +1,110 @@
+#include <Wire.h> // Library for I2C communication
+#include <LiquidCrystal_I2C.h> // Library for LCD
+
+// Wiring: SDA pin is connected to A4 and SCL pin to A5.
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4);
+
+int Score = 0;
+int x;
+int y;
+int a;
+int b;
+int c;
+int d;
+bool Right;
+bool start = true;
+
+void setup() {
+  // Initiate the LCD:
+  lcd.init();
+  lcd.backlight();
+
+  //pins for the joystick
+  pinMode(8,INPUT); 
+  digitalWrite(8,HIGH); 
+}
+
+void loop() {
+  //if we hit the start button
+  if(start){
+    lcd.setCursor(2, 0); // Set the cursor on the third column and first row.
+    lcd.print("Game Started"); // Print the string to the screen
+    delay(2000);
+    lcd.clear(); // clear the screen
+    start = false;
+  }
+    
+  lcd.setCursor(2, 0);
+  lcd.print("Score:     ");
+  lcd.setCursor(2, 1);
+  lcd.print(Score);
+  MixIt();
+}
+
+void DetectAction(bool Right)
+{
+  if(Right){
+    Score++;
+  }
+
+  else
+  {
+    Score = 0;
+    lcd.clear();
+    lcd.setCursor(2, 0); // Set the cursor on the third column and first row.
+    lcd.print("Game Over"); // Print the string to the screen
+    exit(0);
+  }
+}
+
+void MixIt()
+{
+  a = analogRead(A1);
+  if(a == 1023)
+  {
+    delay(120);
+    b = analogRead(A0);  
+    if(b == 1023)
+    { 
+      delay(120);
+      c = analogRead(A1); 
+      if(c == 0)
+      {
+        delay(120);
+        d = analogRead(A0);  
+        if(d == 0)
+        {
+          Right = 1;
+          lcd.setCursor(5, 1);
+          lcd.print(Right);
+          delay(1000);
+          lcd.clear();
+          DetectAction(Right);
+        }
+        else{
+          Right = 0;
+          DetectAction(Right);
+        }
+      }
+      else{
+          Right = 0;
+          DetectAction(Right);
+      }
+    }
+    else{
+          Right = 0;
+          DetectAction(Right);
+    }
+  }
+  
+}
+
+void CoverIt()
+{
+  
+}
+
+void BendIt()
+{
+  
+}
